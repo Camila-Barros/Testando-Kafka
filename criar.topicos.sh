8eb6bc55d344d3500222250fec50561003203c4b
Camila-Barros <camila.barros@me.com>
2023-08-10 00:51:58 -0300

    corrigido o texto do primeiro topico

--- criar.topicos.sh
+++ criar.topicos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear;
 cd /opt/kafka;
-bin/kafka-topics.sh --create --topic MeuSegundoTopico --bootstrap-server localhost:9092;
+bin/kafka-topics.sh --create --topic TopicoTeste --bootstrap-server localhost:9092;
 bin/kafka-topics.sh --create --topic MeuSegundoTopico --bootstrap-server localhost:9092;
 bin/kafka-topics.sh --create --topic MaisUmTopico --bootstrap-server localhost:9092;
 echo "TOPICOS EXISTENTES:";
