b12d18f017b577272076748c4912f5ed63f82a37
Camila-Barros <camila.barros@me.com>
2023-08-10 00:47:27 -0300

    foram criados novos tópicos, um comando limpar tela e um listar tópicos existentes

--- criar.topicos.sh
+++ criar.topicos.sh
@@ -2,6 +2,7 @@
 clear;
 cd /opt/kafka;
 bin/kafka-topics.sh --create --topic MeuSegundoTopico --bootstrap-server localhost:9092;
+bin/kafka-topics.sh --create --topic MeuSegundoTopico --bootstrap-server localhost:9092;
 bin/kafka-topics.sh --create --topic MaisUmTopico --bootstrap-server localhost:9092;
 echo "TOPICOS EXISTENTES:";
 bin/kafka-topics.sh --list --bootstrap-server localhost:9092;
